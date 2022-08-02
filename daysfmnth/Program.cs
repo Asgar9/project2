b9758314dc8e504e37a6950a7b8bb6572bcb4e07
Asgar9 <asgaryas@code.edu.az>
2022-08-02 17:20:50 +0400

    days

---
+++ daysfmnth/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace daysfmnth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = "august";
+            switch (month)
+            {
+                case "january":
+                case "march":
+                case "may":
+                case "july":
+                case "august":
+                case "october":
+                case "december":
+                    Console.WriteLine("ay 31dendir");
+                    break;
+                case "april":
+                case "june":
+                case "september":
+                case "november":
+                    Console.WriteLine("ay 30dandir");
+                    break;
+                case "february":
+                    Console.WriteLine("ay 28den yaxud 29dandir");
+                    break;
+                    Console.ReadLine();
+            }
+        }
+    }
+}
